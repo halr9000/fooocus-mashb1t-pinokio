bb0cd0e18ff85e3e0a9b93c5dbae45a145e4a0d1
Hal Rottenberg <hal@halr9000.com>
2024-10-06 17:30:40 -0400

    Adapted to new pinokio script version

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Fooocus
+# fooocus-mashb1t-pinokio
+
+A pinokio script for https://github.com/mashb1t/Fooocus
 
--- pinokio.js
+++ pinokio.js
@@ -1,99 +1,87 @@
-const path = require("path")
+const path = require('path')
 module.exports = {
   version: "2.0",
-  title: "Fooocus",
-  description: "Minimal Stable Diffusion UI",
-  icon: "icon.jpeg",
-  menu: async (kernel) => {
-
-    // exception handling for windows amd
-    let windowsAmd = (kernel.gpu === "amd" && kernel.platform === "win32")
-    let extraFlags = (windowsAmd ? " --directml --disable-in-browser" : " --disable-in-browser")
-
-    // windows AMD => directml
-    let windowsAMD = (kernel.platform === "win32" && kernel.gpu === "amd")
-    let directml = (windowsAMD ? "--directml " : "")
-    console.log({ windowsAMD, directml })
-
-    let accelerated
-
-    if (kernel.platform === "darwin") {
-      accelerated = kernel.arch === "arm64" // m1/m2/m3
-    } else {
-      accelerated = ["nvidia", "amd"].includes(kernel.gpu)  // gpu is amd/nvidia
+  title: "fooocus-mashb1t-pinokio",
+  description: "",
+  icon: "icon.png",
+  menu: async (kernel, info) => {
+    let installed = info.exists("app/env")
+    let running = {
+      install: info.running("install.js"),
+      start: info.running("start.js"),
+      update: info.running("update.js"),
+      reset: info.running("reset.js")
     }
-
-    let alwaysCPU = (accelerated ? "" : "--always-cpu ")
-
-    let installing = kernel.running(__dirname, "install.json")
-    let installed = await kernel.exists(__dirname, "app", "env")
-
-    if (installing) {
-      return [{ default: true, icon: "fa-solid fa-plug", text: "Installing...", href: "install.json" }]
+    if (running.install) {
+      return [{
+        default: true,
+        icon: "fa-solid fa-plug",
+        text: "Installing",
+        href: "install.js",
+      }]
     } else if (installed) {
-      let running = kernel.running(__dirname, "start.json")
-      let updating = kernel.running(__dirname, "update.json")
-      let resetting = kernel.running(__dirname, "reset.json")
-      if (running) {
-        let memory = kernel.memory.local[path.resolve(__dirname, "start.json")]
-        if (memory && memory.url) {
-          return [
-            { default: true, icon: "fa-solid fa-rocket", text: "Web UI", href: memory.url },
-            { icon: "fa-solid fa-terminal", text: "Terminal", href: "start.json" },
-            { icon: "fa-solid fa-rotate", text: "Update", href: "update.json" },
-          ]
+      if (running.start) {
+        let local = info.local("start.js")
+        if (local && local.url) {
+          return [{
+            default: true,
+            icon: "fa-solid fa-rocket",
+            text: "Open Web UI",
+            href: local.url,
+          }, {
+            icon: 'fa-solid fa-terminal',
+            text: "Terminal",
+            href: "start.js",
+          }]
         } else {
-          return [
-            { default: true, icon: "fa-solid fa-terminal", text: "Terminal", href: "start.json" },
-            { icon: "fa-solid fa-rotate", text: "Update", href: "update.json" },
-          ]
+          return [{
+            default: true,
+            icon: 'fa-solid fa-terminal',
+            text: "Terminal",
+            href: "start.js",
+          }]
         }
-      } else if (updating) {
+      } else if (running.update) {
         return [{
-          icon: "fa-solid fa-power-off",
-          text: "Start",
-          href: "start.json",
-          params: { flags: `${alwaysCPU}${directml}--preset default${extraFlags}` }
-        }, {
-          default: true, icon: "fa-solid fa-rotate", text: "Updating", href: "update.json"
-        }, {
-          icon: "fa-solid fa-plug", text: "Reinstall", href: "install.json"
-       }, {
-          icon: "fa-solid fa-circle-xmark", text: "Reset", href: "reset.json", confirm: "Are you sure you wish to reset the app?"
+          default: true,
+          icon: 'fa-solid fa-terminal',
+          text: "Updating",
+          href: "update.js",
         }]
-      } else if (resetting) {
+      } else if (running.reset) {
         return [{
-          icon: "fa-solid fa-power-off",
-          text: "Start",
-          href: "start.json",
-          params: { flags: `${alwaysCPU}${directml}--preset default${extraFlags}` }
-        }, {
-          icon: "fa-solid fa-rotate", text: "Updating", href: "update.json"
-        }, {
-          icon: "fa-solid fa-plug", text: "Reinstall", href: "install.json"
-       }, {
-          default: true, icon: "fa-solid fa-circle-xmark", text: "Resetting", href: "reset.json", confirm: "Are you sure you wish to reset the app?"
+          default: true,
+          icon: 'fa-solid fa-terminal',
+          text: "Resetting",
+          href: "reset.js",
         }]
       } else {
         return [{
           default: true,
           icon: "fa-solid fa-power-off",
           text: "Start",
-          href: "start.json",
-          params: { flags: `${alwaysCPU}${directml}--preset default${extraFlags}` }
+          href: "start.js",
         }, {
-          icon: "fa-solid fa-rotate", text: "Update", href: "update.json"
+          icon: "fa-solid fa-plug",
+          text: "Update",
+          href: "update.js",
         }, {
-          icon: "fa-solid fa-plug", text: "Reinstall", href: "install.json"
-       }, {
-          icon: "fa-solid fa-circle-xmark", text: "Reset", href: "reset.json", confirm: "Are you sure you wish to reset the app?"
+          icon: "fa-solid fa-plug",
+          text: "Install",
+          href: "install.js",
+        }, {
+          icon: "fa-regular fa-circle-xmark",
+          text: "Reset",
+          href: "reset.js",
         }]
       }
     } else {
-      return [
-        { default: true, icon: "fa-solid fa-plug", text: "Install", href: "install.json" },
-        { icon: "fa-solid fa-rotate", text: "Update", href: "update.json" }
-      ]
+      return [{
+        default: true,
+        icon: "fa-solid fa-plug",
+        text: "Install",
+        href: "install.js",
+      }]
     }
   }
 }
